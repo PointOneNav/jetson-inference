e983d83053e36a891e9c760609de8318d8d9b40a
Pierce Cohen <piercecohen1@gmail.com>
2019-08-01 17:36:59 -0700

    Updates to support x86_64 architectures

--- tools/setup_orb.sh
+++ tools/setup_orb.sh
@@ -124,8 +124,100 @@ if [[ `uname` == 'Linux' && `uname -p` == "aarch64" ]]; then
 	bazel run -c opt //point_one/vision/maply/localization:orb -- --map ~/Downloads/map\ \(2\).bin --calibrationSFM ~/calibration/camera_calibration_flir_17391313.xml -pyramid 1  --alignment ~/Downloads/sim_unscaled.sim --origin ~/Downloads/origin\ \(2\).json
 
 elif [[ `uname` == 'Linux' && `uname -p` == "x86_64" ]]; then
-	### Code for x86_64 ###
-	# sudo cp /usr/lib/x86_64-linux-gnu/gstreamer-1.0/include/gst/* /usr/include/gstreamer-1.0/gst/
+        ### Install required dependencies from apt ###
+        sudo apt-get update
+        sudo apt install -y libwayland-dev libegl1-mesa-dev libxkbcommon-dev libdc1394-22-dev libgeographic-dev libsuitesparse-dev libjpeg-dev libturbojpeg0-dev libtiff-dev libflann-dev libglfw3-dev libglm-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev libglew-dev
+        sudo apt-get -y install git g++-5 gcc-5 default-jdk libxkbcommon-dev cmake python-pip python3-pip libboost-all-dev cmake
+
+	### Copy gstreamer files ###
+	sudo cp /usr/lib/x86_64-linux-gnu/gstreamer-1.0/include/gst/* /usr/include/gstreamer-1.0/gst/
+
+	### Download appropriate nv-tensorrt library for OS ###
+	cd $HOME
+
+	lsb_release -r | grep -q '16.04'
+	if [ $? == 0 ]; then
+                echo "Fetching nv-tensorrt for Ubuntu 16.04..."
+                wget https://pointone-public-build-assets.s3-us-west-1.amazonaws.com/nvidia/nv-tensorrt-repo-ubuntu1604-cuda10.1-trt5.1.5.0-ga-20190427_1-1_amd64.deb
+                sudo dpkg -i nv-tensorrt-repo-ubuntu1604-cuda10.1-trt5.1.5.0-ga-20190427_1-1_amd64.deb
+	else
+		lsb_release -r | grep -q '18.04'
+        	if [ $? == 0 ]; then
+                	echo "Fetching nv-tensorrt for Ubuntu 18.04..."
+                	wget https://pointone-public-build-assets.s3-us-west-1.amazonaws.com/nvidia/nv-tensorrt-repo-ubuntu1804-cuda10.1-trt5.1.5.0-ga-20190427_1-1_amd64.deb
+                	sudo dpkg -i nv-tensorrt-repo-ubuntu1804-cuda10.1-trt5.1.5.0-ga-20190427_1-1_amd64.deb
+		else
+			echo "Error: Unsupported OS for nv-tensorrt"
+			exit 1
+		fi
+	fi
+	
+	sudo apt-key add /var/nv-tensorrt-repo-cuda10.1-trt5.1.5.0-ga-20190427/7fa2af80.pub
+	sudo apt-get update
+	sudo apt-get install tensorrt
+
+        ### Download and install Bazel 0.22 ###
+        cd $HOME
+	wget https://github.com/bazelbuild/bazel/releases/download/0.22.0/bazel-0.22.0-installer-linux-x86_64.sh
+        chmod +x bazel-0.22.0-installer-linux-x86_64.sh
+	./bazel-0.22.0-installer-linux-x86_64.sh
+        source $HOME/.bashrc
+
+	### Clone and setup Nautilus libraries ###
+	cd $HOME
+	git clone --branch lucas/orb_gpu_features https://github.com/PointOneNav/nautilus.git
+	echo 'export REPO_ROOT=$HOME/nautilus' >> $HOME/.bashrc
+	source $HOME/.bashrc
+	cd $HOME/nautilus/
+	./tools/setup/setup_libraries.sh # Install 3rd-party dependency libraries
+
+	### Make and install OpenCV ###
+	cd $HOME
+	wget https://github.com/opencv/opencv/archive/3.4.2.zip?source=post_page--------------------------- -O opencv-3.4.2.zip
+	unzip opencv-3.4.2.zip
+	mkdir $HOME/opencv-3.4.2/build/
+	cd $HOME/opencv-3.4.2/build/
+	cmake -D CMAKE_BUILD_TYPE=Release -D BUILD_PNG=OFF -D BUILD_TIFF=OFF -D BUILD_TBB=OFF -D BUILD_JPEG=OFF -D BUILD_JASPER=OFF -D BUILD_ZLIB=OFF -D BUILD_EXAMPLES=OFF -D BUILD_opencv_java=OFF -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=OFF -D ENABLE_NEON=ON -D WITH_OPENCL=OFF -D WITH_OPENMP=OFF -D WITH_FFMPEG=ON -D WITH_GSTREAMER=OFF -D WITH_GSTREAMER_0_10=OFF -D WITH_CUDA=ON -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda -D WITH_GTK=OFF -D WITH_VTK=OFF -D WITH_TBB=ON -D WITH_1394=OFF -D WITH_OPENEXR=OFF -D CUDA_ARCH_BIN=6.0 6.1 7.0 -D CUDA_ARCH_PTX="" -D INSTALL_C_EXAMPLES=OFF -D INSTALL_TESTS=OFF  -D BUILD_opencv_cudacodec=OFF ..
+	make -j8
+	sudo make install
+
+	### Setup GPSTk ###
+	cd $HOME/nautilus/
+	./tools/setup/setup_gpstk.sh
+
+	### Make and install websocketpp ###
+	cd $HOME
+	git clone git://github.com/zaphoyd/websocketpp.git
+	cd websocketpp/
+	cmake .
+	sudo make install
+
+	### Clone and setup jetson-inference repo ###
+	cd $HOME/nautilus/third_party/
+	git clone https://github.com/PointOneNav/jetson-inference.git
+	cd $HOME/nautilus/third_party/jetson-inference/
+	git submodule update --init
+	mkdir build
+	cd build
+	cmake ../
+	make -j8
+
+	### Clone and setup gpu_orb_extractor ###
+	cd $HOME/nautilus/third_party/
+	git clone https://github.com/PointOneNav/gpu_orb_extractor.git
+	cd gpu_orb_extractor/
+	cmake .
+	make -j8
+
+	### Fetch ORBvoc.txt ###
+	mkdir $HOME/nautilus/third_party/ORBSLAM2/Vocabulary/
+	cd $HOME/nautilus/third_party/ORBSLAM2/Vocabulary/
+	wget https://pointone-vision.s3-us-west-1.amazonaws.com/models/vocabulary/ORBvoc.txt.tar.gz
+	tar xvf ORBvoc.txt.tar.gz
+	rm ORBvoc.txt.tar.gz
+
+	### Clean up ###
+	
 else
     echo "Not supported architecture"
 fi
