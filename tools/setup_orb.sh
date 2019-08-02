f84167d2be5ad80211cd4e996f8f54ada2341eac
Pierce Cohen <piercecohen1@gmail.com>
2019-08-02 14:29:54 -0700

    Move repeated code into functions. Rename build_jetson_armv8.sh to build_jetson.sh .

--- tools/setup_orb.sh
+++ tools/setup_orb.sh
@@ -1,138 +1,87 @@
 #!/usr/bin/env bash
 
-if [[ `uname` == 'Linux' && `uname -p` == "aarch64" ]]; then
-	sudo /usr/bin/jetson_clocks    # (max out fans)
-	sudo nvpmodel -q     # (query the current mode)
-	sudo nvpmodel -m 0   # (enable MAX-N)
+set -x
 
-	### Add line to Crontab to max out fans at boot ###
-	cd $HOME
-	line="@reboot  /usr/bin/jetson_clocks"
-	sudo crontab -l > mycron # write out current crontab
-	echo $line >> mycron
-	sudo crontab mycron # install new cron file
-	rm mycron
+install_opencv () {
+        cd $HOME
+        wget https://github.com/opencv/opencv/archive/3.4.2.zip?source=post_page--------------------------- -O opencv-3.4.2.zip
+        unzip opencv-3.4.2.zip
+        mkdir $HOME/opencv-3.4.2/build/
+        cd $HOME/opencv-3.4.2/build/
+        cmake -D CMAKE_BUILD_TYPE=Release -D BUILD_PNG=OFF -D BUILD_TIFF=OFF -D BUILD_TBB=OFF -D BUILD_JPEG=OFF -D BUILD_JASPER=OFF -D BUILD_ZLIB=OFF -D BUILD_EXAMPLES=OFF -D BUILD_opencv_java=OFF -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=OFF -D ENABLE_NEON=ON -D WITH_OPENCL=OFF -D WITH_OPENMP=OFF -D WITH_FFMPEG=ON -D WITH_GSTREAMER=OFF -D WITH_GSTREAMER_0_10=OFF -D WITH_CUDA=ON -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda -D WITH_GTK=ON -D WITH_VTK=OFF -D WITH_TBB=ON -D WITH_1394=OFF -D WITH_OPENEXR=OFF -D CUDA_ARCH_BIN=6.0 6.1 7.0 -D CUDA_ARCH_PTX="" -D INSTALL_C_EXAMPLES=OFF -D INSTALL_TESTS=OFF  -D BUILD_opencv_cudacodec=OFF ..
+        make -j8
+        sudo make install
+}
 
-	### Install required dependencies from apt ###
-	sudo apt-get update
-	sudo apt install -y libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev libglew-dev
-	sudo apt-get -y install g++-5 gcc-5 default-jdk libxkbcommon-dev cmake python-pip python3-pip libboost-all-dev
+setup_jetson-inference () {
+        cd $HOME/nautilus/third_party/
+        if [ ! -d "jetson-inference" ]; then
+                git clone https://github.com/PointOneNav/jetson-inference.git
+        fi
 
-	### Download and build Bazel from source ###
-	cd $HOME
-	wget https://github.com/bazelbuild/bazel/releases/download/0.22.0/bazel-0.22.0-dist.zip
-	mkdir bazel-extract
-	unzip bazel-0.22.0-dist.zip -d $HOME/bazel-extract/
-	$HOME/bazel-extract/compile.sh
-	echo 'export PATH=$HOME/bazel-extract/output:$PATH' >> $HOME/.bashrc
-	source $HOME/.bashrc
-
-	### Clone and setup Nautilus libraries ###
-	cd $HOME
-	git clone --branch lucas/orb_gpu_features https://github.com/PointOneNav/nautilus.git
-	echo 'export REPO_ROOT=$HOME/nautilus' >> $HOME/.bashrc
-	source $HOME/.bashrc
-	cd $HOME/nautilus/
-	./tools/setup/setup_libraries.sh # Install 3rd-party dependency libraries
-
-	### Make and install OpenCV ###
-	cd $HOME
-	wget https://github.com/opencv/opencv/archive/3.4.2.zip?source=post_page--------------------------- -O opencv-3.4.2.zip
-	unzip opencv-3.4.2.zip
-	mkdir $HOME/opencv-3.4.2/build/
-	cd $HOME/opencv-3.4.2/build/
-	cmake -D CMAKE_BUILD_TYPE=Release -D BUILD_PNG=OFF -D BUILD_TIFF=OFF -D BUILD_TBB=OFF -D BUILD_JPEG=OFF -D BUILD_JASPER=OFF -D BUILD_ZLIB=OFF -D BUILD_EXAMPLES=OFF -D BUILD_opencv_java=OFF -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=OFF -D ENABLE_NEON=ON -D WITH_OPENCL=OFF -D WITH_OPENMP=OFF -D WITH_FFMPEG=ON -D WITH_GSTREAMER=OFF -D WITH_GSTREAMER_0_10=OFF -D WITH_CUDA=ON -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda -D WITH_GTK=OFF -D WITH_VTK=OFF -D WITH_TBB=ON -D WITH_1394=OFF -D WITH_OPENEXR=OFF -D CUDA_ARCH_BIN=6.0 6.1 7.0 -D CUDA_ARCH_PTX="" -D INSTALL_C_EXAMPLES=OFF -D INSTALL_TESTS=OFF  -D BUILD_opencv_cudacodec=OFF ..
-	make -j8
-	sudo make install
-
-	### Setup GPSTk ###
-	cd $HOME/nautilus/
-	set -ex
-
-	if [ ! -e "WORKSPACE" ]; then
-		echo "SHOULD BE RUN FROM THE ROOT OF WORKSPACE"
-		exit 1
-	fi
-
-	cd third_party
-	    if [ ! -d "GPSTk" ]; then
-		git clone https://github.com/PointOneNav/GPSTk.git
-		cd GPSTk
-	    else
-		cd GPSTk
-		git fetch
-	    fi
-		git checkout 35691d368f6d3669931572c9df8a90a050e19d03
-		if [ ! -d "build" ]; then
-		    mkdir build
-		fi
-		cd build
-		    cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local
-		    make -j8
-		    sudo make install
-		cd ..
-		rm -rf build
-		export PATH=$PATH:/opt/local/gcc-linaro/bin
-	     cd ..
-	     rm -rf GPSTk
-	cd ..
-
-	### Make and install websocketpp ###
-	cd $HOME
-	git clone git://github.com/zaphoyd/websocketpp.git
-	cd websocketpp/
-	cmake .
-	sudo make install
-
-	### Clone and setup jetson-inference repo ###
-	cd $HOME/nautilus/third_party/
-	git clone https://github.com/PointOneNav/jetson-inference.git
-	cd $HOME/nautilus/third_party/jetson-inference/
+	cd jetson-inference
 	git submodule update --init
 	mkdir build
 	cd build
 	cmake ../
 	make -j8
-	sudo make install
+}
 
-	### Clone and setup gpu_orb_extractor ###
-	cd $HOME/nautilus/third_party/
-	git clone https://github.com/PointOneNav/gpu_orb_extractor.git
-	cd gpu_orb_extractor/
-	cmake .
-	make -j8
+fetch_orbvoc () {
+        cd $HOME/nautilus/third_party/ORBSLAM2/
+        if [ ! -d "Vocabulary" ]; then
+                mkdir ./Vocabulary/
+        fi
+        cd Vocabulary
+        if [ ! -e "ORBvoc.txt" ]; then
+                wget https://pointone-vision.s3-us-west-1.amazonaws.com/models/vocabulary/ORBvoc.txt.tar.gz
+                tar xvf ORBvoc.txt.tar.gz
+                rm ORBvoc.txt.tar.gz
+        fi
+}
 
-	### Fetch ORBvoc.txt ###
-	mkdir $HOME/nautilus/third_party/ORBSLAM2/Vocabulary/
-	cd $HOME/nautilus/third_party/ORBSLAM2/Vocabulary/
-	wget https://pointone-vision.s3-us-west-1.amazonaws.com/models/vocabulary/ORBvoc.txt.tar.gz
-	tar xvf ORBvoc.txt.tar.gz
-	rm ORBvoc.txt.tar.gz
+setup_gpstk_agx () {
+        cd $HOME/nautilus/
 
-	### Setup environment ###
-	cd $HOME/nautilus/third_party/jetson-inference
-	./build_jetson_armv8.sh
+        if [ ! -e "WORKSPACE" ]; then
+                echo "SHOULD BE RUN FROM THE ROOT OF WORKSPACE"
+                exit 1
+        fi
 
-	### Clean up ###
+        cd third_party
+            if [ ! -d "GPSTk" ]; then
+                git clone https://github.com/PointOneNav/GPSTk.git
+                cd GPSTk
+            else
+                cd GPSTk
+                git fetch
+            fi
+                git checkout 35691d368f6d3669931572c9df8a90a050e19d03
+                if [ ! -d "build" ]; then
+                    mkdir build
+                fi
+                cd build
+                    cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local
+                    make -j8
+                    sudo make install
+                cd ..
+                rm -rf build
+                export PATH=$PATH:/opt/local/gcc-linaro/bin
+             cd ..
+             rm -rf GPSTk
+        cd ..
+}
+
+setup_nautilus () {
 	cd $HOME
-	rm bazel-0.22.0-dist.zip
-	rm opencv-3.4.2.zip
+	if [ ! -d "nautilus" ]; then
+		git clone --branch lucas/orb_gpu_features https://github.com/PointOneNav/nautilus.git
+		echo 'export REPO_ROOT=$HOME/nautilus' >> $HOME/.bashrc
+		source $HOME/.bashrc
+	fi
+}
 
-	### Build ORB ###
-	cd $HOME/nautilus
-	source $HOME/.bashrc
-	bazel run -c opt //point_one/vision/maply/localization:orb -- --map ~/Downloads/map\ \(2\).bin --calibrationSFM ~/calibration/camera_calibration_flir_17391313.xml -pyramid 1  --alignment ~/Downloads/sim_unscaled.sim --origin ~/Downloads/origin\ \(2\).json
-
-elif [[ `uname` == 'Linux' && `uname -p` == "x86_64" ]]; then
-        ### Install required dependencies from apt ###
-        sudo apt-get update
-        sudo apt install -y libwayland-dev libegl1-mesa-dev libxkbcommon-dev libdc1394-22-dev libgeographic-dev libsuitesparse-dev libjpeg-dev libturbojpeg0-dev libtiff-dev libflann-dev libglfw3-dev libglm-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev libglew-dev
-        sudo apt-get -y install git g++-5 gcc-5 default-jdk libxkbcommon-dev cmake python-pip python3-pip libboost-all-dev cmake
-
-	### Copy gstreamer files ###
-	sudo cp /usr/lib/x86_64-linux-gnu/gstreamer-1.0/include/gst/* /usr/include/gstreamer-1.0/gst/
-
-	### Download appropriate nv-tensorrt library for OS ###
+setup_tensorrt () {
 	cd $HOME
 
 	lsb_release -r | grep -q '16.04'
@@ -151,11 +100,94 @@ elif [[ `uname` == 'Linux' && `uname -p` == "x86_64" ]]; then
 			exit 1
 		fi
 	fi
-	
-	sudo apt-key add /var/nv-tensorrt-repo-cuda10.1-trt5.1.5.0-ga-20190427/7fa2af80.pub
-	sudo apt-get update
-	sudo apt-get install tensorrt
 
+        sudo apt-key add /var/nv-tensorrt-repo-cuda10.1-trt5.1.5.0-ga-20190427/7fa2af80.pub
+        sudo apt-get update
+        sudo apt-get install tensorrt
+}
+
+setup_gpu_orb_extractor () {
+	cd $HOME/nautilus/third_party/
+        if [ ! -d "gpu_orb_extractor" ]; then
+                git clone https://github.com/PointOneNav/gpu_orb_extractor.git
+        fi
+
+	cd gpu_orb_extractor/
+	cmake .
+	make -j8
+}
+
+if [[ `uname` == 'Linux' && `uname -p` == "aarch64" ]]; then
+	sudo /usr/bin/jetson_clocks    # (max out fans)
+	sudo nvpmodel -q     # (query the current mode)
+	sudo nvpmodel -m 0   # (enable MAX-N)
+
+	### Add line to Crontab to max out fans at boot ###
+	cd $HOME
+	line="@reboot  /usr/bin/jetson_clocks"
+	sudo crontab -l > mycron # write out current crontab
+	echo $line >> mycron
+	sudo crontab mycron # install new cron file
+	rm mycron
+
+	### Install required dependencies from apt ###
+	sudo apt-get update
+	sudo apt install -y libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev libglew-dev
+	sudo apt-get -y install g++-5 gcc-5 default-jdk libxkbcommon-dev cmake python-pip python3-pip libboost-all-dev libwebsocketpp-dev
+
+	### Download and build Bazel from source ###
+	cd $HOME
+	wget https://github.com/bazelbuild/bazel/releases/download/0.22.0/bazel-0.22.0-dist.zip
+	mkdir bazel-extract
+	unzip bazel-0.22.0-dist.zip -d $HOME/bazel-extract/
+	$HOME/bazel-extract/compile.sh
+	echo 'export PATH=$HOME/bazel-extract/output:$PATH' >> $HOME/.bashrc
+	source $HOME/.bashrc
+
+	### Clone and setup Nautilus libraries ###
+	setup_nautilus
+
+	### Install 3rd-party dependency libraries
+	cd $HOME/nautilus/
+	./tools/setup/setup_libraries.sh # Install 3rd-party dependency libraries
+
+	### Install opencv ###
+	install_opencv
+
+	### Setup GPSTk ###
+	setup_gpstk_agx
+
+	### Clone and setup jetson-inference repo ###
+	setup_jetson-inference
+
+	### Clone and setup gpu_orb_extractor ###
+	setup_gpu_orb_extractor
+
+	### Fetch ORBvoc.txt ###
+	fetch_orbvoc
+
+	### Setup environment ###
+	cd $HOME/nautilus/third_party/jetson-inference
+	./build_jetson.sh
+
+	### Clean up ###
+	cd $HOME
+	rm bazel-0.22.0-dist.zip
+	rm opencv-3.4.2.zip
+
+
+elif [[ `uname` == 'Linux' && `uname -p` == "x86_64" ]]; then
+        ### Install required dependencies from apt ###
+        sudo apt-get update
+        sudo apt install -y libwayland-dev libegl1-mesa-dev libxkbcommon-dev libdc1394-22-dev libgeographic-dev libsuitesparse-dev libjpeg-dev libturbojpeg0-dev libtiff-dev libflann-dev libglfw3-dev libglm-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev libglew-dev
+        sudo apt-get -y install git g++-5 gcc-5 default-jdk libxkbcommon-dev cmake python-pip python3-pip libboost-all-dev cmake libwebsocketpp-dev
+
+	### Copy gstreamer files ###
+	sudo cp /usr/lib/x86_64-linux-gnu/gstreamer-1.0/include/gst/* /usr/include/gstreamer-1.0/gst/
+
+	### Setup appropriate nv-tensorrt library for OS ###
+	setup_tensorrt
+	
         ### Download and install Bazel 0.22, if not already installed ###
         bazel version | grep -q 'Build label: 0.22'
 	if [ $? > 0 ]; then
@@ -167,76 +199,27 @@ elif [[ `uname` == 'Linux' && `uname -p` == "x86_64" ]]; then
 	fi
 
 	### Clone and setup Nautilus repo if it does not exist ###
-	cd $HOME
-	if [ ! -d "nautilus" ]; then
-		git clone --branch lucas/orb_gpu_features https://github.com/PointOneNav/nautilus.git
-		echo 'export REPO_ROOT=$HOME/nautilus' >> $HOME/.bashrc
-		source $HOME/.bashrc
-	fi
+	setup_nautilus
 
 	### Install 3rd-party dependency libraries ###
 	cd $HOME/nautilus/
 	./tools/setup/setup_libraries.sh
 
-	### Make and install OpenCV ###
-	cd $HOME
-	wget https://github.com/opencv/opencv/archive/3.4.2.zip?source=post_page--------------------------- -O opencv-3.4.2.zip
-	unzip opencv-3.4.2.zip
-	mkdir $HOME/opencv-3.4.2/build/
-	cd $HOME/opencv-3.4.2/build/
-	cmake -D CMAKE_BUILD_TYPE=Release -D BUILD_PNG=OFF -D BUILD_TIFF=OFF -D BUILD_TBB=OFF -D BUILD_JPEG=OFF -D BUILD_JASPER=OFF -D BUILD_ZLIB=OFF -D BUILD_EXAMPLES=OFF -D BUILD_opencv_java=OFF -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=OFF -D ENABLE_NEON=ON -D WITH_OPENCL=OFF -D WITH_OPENMP=OFF -D WITH_FFMPEG=ON -D WITH_GSTREAMER=OFF -D WITH_GSTREAMER_0_10=OFF -D WITH_CUDA=ON -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda -D WITH_GTK=OFF -D WITH_VTK=OFF -D WITH_TBB=ON -D WITH_1394=OFF -D WITH_OPENEXR=OFF -D CUDA_ARCH_BIN=6.0 6.1 7.0 -D CUDA_ARCH_PTX="" -D INSTALL_C_EXAMPLES=OFF -D INSTALL_TESTS=OFF  -D BUILD_opencv_cudacodec=OFF ..
-	make -j8
-	sudo make install
+        ### Install opencv ###
+	install_opencv
 
 	### Setup GPSTk ###
 	cd $HOME/nautilus/
 	./tools/setup/setup_gpstk.sh
 
-	### Make and install websocketpp in home directory if it does not exist ###
-	cd $HOME
-	if [ ! -d "websocketpp" ]; then
-		git clone git://github.com/zaphoyd/websocketpp.git
-		cd websocketpp/
-		cmake .
-		sudo make install
-	fi
-
 	### Clone jetson-inference if it does not exist ###
-	cd $HOME/nautilus/third_party/
-	if [ ! -d "jetson-inference" ]; then
-		git clone https://github.com/PointOneNav/jetson-inference.git
-	fi
+	setup_jetson-inference	
 	
-	### Make jetson-inference ###
-	cd $HOME/nautilus/third_party/jetson-inference/
-	git submodule update --init
-	mkdir build
-	cd build
-	cmake ../
-	make -j8
-
 	### Clone gpu_orb_extractor if it does not exist ###
-	cd $HOME/nautilus/third_party/
-        if [ ! -d "gpu_orb_extractor" ]; then
-                git clone https://github.com/PointOneNav/gpu_orb_extractor.git
-        fi
-
-	### Make gpu_orb_extractor ###
-	cd gpu_orb_extractor/
-	cmake .
-	make -j8
+	setup_gpu_orb_extractor
 
 	### Fetch ORBvoc.txt if it does not exist ###
-	cd $HOME/nautilus/third_party/ORBSLAM2/
-	if [ ! -d "Vocabulary" ]; then
-		mkdir ./Vocabulary/
-	fi
-	cd Vocabulary
-	if [ ! -e "ORBvoc.txt" ]; then
-		wget https://pointone-vision.s3-us-west-1.amazonaws.com/models/vocabulary/ORBvoc.txt.tar.gz
-		tar xvf ORBvoc.txt.tar.gz
-		rm ORBvoc.txt.tar.gz
-	fi
+	fetch_orbvoc
 
 	### Clean up ###
 	rm $HOME/opencv-3.4.2.zip
